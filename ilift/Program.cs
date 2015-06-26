9ba52a0f189179d196426880bca617524ce4a944
Floriment <klinakuf@gmail.com>
2015-06-26 18:02:57 +0200

    Cleaned up Program

--- ilift/Program.cs
+++ ilift/Program.cs
@@ -21,82 +21,36 @@ using ilift.Controller;
 
 namespace ilift
 {
+    /// <summary>
+    /// Program entry point.
+    /// </summary>
     public partial class Program : HardwareController
     {
-        BicepCurl bicepCurl;
-        LateralRaise lateralRaise;
-        // This method is run when the mainboard is powered up or reset.   
+        /// <summary>
+        /// This method is run when the mainboard is powered up or reset.
+        /// </summary>
         void ProgramStarted()
         {
             Debug.Print("Program started");
             AccelerometerInit();
-           
             NetworkClient.ConnectToWifi(wifiRS21,OnNetworkUp);
         }
 
+        /// <summary>
+        /// Initialize the accelerometer.
+        /// </summary>
         private void AccelerometerInit() {
             accelerometer.MeasurementInterval = new TimeSpan(0, 0, 0, 0, 100);
         }
 
+        /// <summary>
+        /// Start the app controller once the network is up.
+        /// </summary>
         private void OnNetworkUp()
         {
             Debug.Print("Now we are Network UP");
             AppController appController = new AppController(this);
 
-        }
-
-    
-
-        private void rfidReader_IdReceived(RFIDReader sender, string e)
-        {
-            Debug.Print("RFID scanned: " + e);
-            NetworkClient.GetUser(e,user => 
-                    Debug.Print("This user is : " + user.username)
-                );
-        }
-
-        private void rfidReader_MalformedIdReceived(RFIDReader sender, EventArgs e)
-        {
-            Debug.Print("Please rescan your card");
-        }
-
-        void bicepCurl_onRepetitionDone()
-        {
-            Debug.Print("Repetition Done!");
-        }
-
-        void compass_MeasurementComplete(GTM.GHIElectronics.Compass sender, 
-            GTM.GHIElectronics.Compass.MeasurementCompleteEventArgs e)
-        {
-            //Debug.Print("Compass:\tx: " + e.X + "\ty: " + e.Y + "\tz: " + e.Z + "\n");
-        }
-
-        //void button_ButtonPressed(GTM.GHIElectronics.Button sender, GTM.GHIElectronics.Button.ButtonState state)
-        //{
-        //    //compass.StartTakingMeasurements();
-
-        //    accelerometer.Calibrate();
-        //    accelerometer.StartTakingMeasurements();
-        //    Debug.Print("Calibrated");
-        //}
-
-        void accelerometer_MeasurementComplete(
-            GTM.GHIElectronics.Accelerometer sender, 
-            GTM.GHIElectronics.Accelerometer.MeasurementCompleteEventArgs e)
-        {
-            //bicepCurl.processData(e.X, e.Y, e.Z);
-            lateralRaise.ProcessData(e.X, e.Y, e.Z);
-        }
-
-        //public void RegisterButtonPressedHandler(Button.ButtonEventHandler handler)
-        //{
-        //    button.ButtonPressed += handler;
-        //}
-
-        public void RegisterDisplayTouchedHandler(GUITouchDelegate handler)
-        {
-            // TODO
-            //throw new NotImplementedException();
         }
 
         public void RegisterRFIDReadHandler(RFIDReader.IdReceivedEventHandler handler)
