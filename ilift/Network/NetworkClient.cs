3ab236642b696441f66c8c2e8c26711c622acf58
Floriment <klinakuf@gmail.com>
2015-06-30 16:33:01 +0200

    Network Client commented

--- ilift/Network/NetworkClient.cs
+++ ilift/Network/NetworkClient.cs
@@ -13,14 +13,22 @@ namespace ilift.Network
     public delegate void EquipmentReply(Equipment equipment);
     public delegate void NetworkConnected();
   
-
+    /// <summary>
+    /// NetworkClient that connects to WiFi and offers possibilities to communicate with the REST service
+    /// </summary>
     public class NetworkClient
     {
+        //the address of the REST service
         public const string ADDRESS = "http://192.168.43.245:8080/ilift/";
+        //login credentials wifi
         private const string SSID = "AndroidAP";
         private const string KEY = "tk3-umundo";
 
-
+        /// <summary>
+        /// Opens hardware network interface and tries to connect with the SSID and KEY
+        /// </summary>
+        /// <param name="wifi">The wifi hardware</param>
+        /// <param name="networkConnectedDelegate">Delegate after the wifi is set up and network is up and running</param>
         public static void ConnectToWifi(WiFiRS21 wifi,NetworkConnected networkConnectedDelegate)
         {
             wifi.NetworkInterface.Open();
@@ -41,6 +49,11 @@ namespace ilift.Network
             
         }
 
+        /// <summary>
+        /// Get the user with specified rfid.
+        /// </summary>
+        /// <param name="rfid">Rfid scanned</param>
+        /// <param name="userReplyDelegate">Delegate the execution after the response</param>
         public static void GetUser(string rfid, UserReply userReplyDelegate)
         {
             HttpRequest wc = WebClient.GetFromWeb(ADDRESS + 
@@ -60,6 +73,11 @@ namespace ilift.Network
             };
         }
 
+        /// <summary>
+        /// Get the equipment with the scanned rfid tag 
+        /// </summary>
+        /// <param name="tag">Rfid tag</param>
+        /// <param name="equipmentReplyDelegate">Delegate the execution after the response</param>
         public static void GetEquipmentByTag(string tag, EquipmentReply equipmentReplyDelegate)
         {
             HttpRequest wc = WebClient.GetFromWeb(ADDRESS +
@@ -79,6 +97,10 @@ namespace ilift.Network
             };
         }
 
+        /// <summary>
+        /// Issue a post request to insert the given session
+        /// </summary>
+        /// <param name="session">The given session</param>
         public static void PostSession(Session session)
         {
             POSTContent pc =
