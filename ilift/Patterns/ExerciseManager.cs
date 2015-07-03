bc427b8d393309ee75dd1df7cdf7b79547cd6595
Chen, Yiqun <feedingfranzy@gmail.com>
2015-07-03 14:45:18 +0200

    [comment] Add comments on exercise manager

--- ilift/Patterns/ExerciseManager.cs
+++ ilift/Patterns/ExerciseManager.cs
@@ -4,10 +4,20 @@ using System.Collections;
 
 namespace ilift.Patterns
 {
+    /// <summary> 
+    /// The exercise manager that is responsible for retrieving 
+    /// exercise from server
+    /// </summary>
     public class ExerciseManager
     {
+        /// <summary>
+        /// All available exercises
+        /// </summary>
         private static Hashtable _exercises = new Hashtable();
 
+        /// <summary>
+        /// The constructor that initialize all supported exercises
+        /// </summary>
         static ExerciseManager()
         {
             _exercises[1L] = new BicepCurl();
@@ -15,6 +25,11 @@ namespace ilift.Patterns
             _exercises[3L] = new TricepExtension();
         }
 
+        /// <summary>
+        /// Fetching the exercise by id
+        /// </summary>
+        /// <param name="id"> Exercise Id </param>
+        /// <return> The exercise object </return>
         public static AbstractExercise GetExercise(long id)
         {
             return (AbstractExercise)_exercises[id];
