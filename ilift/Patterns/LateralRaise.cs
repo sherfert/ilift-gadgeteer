b473a5993c4d3ee640b604bbc83672473296c185
Chen, Yiqun <feedingfranzy@gmail.com>
2015-07-03 14:33:06 +0200

    [comment] Add comments on action patterns

--- ilift/Patterns/LateralRaise.cs
+++ ilift/Patterns/LateralRaise.cs
@@ -4,11 +4,19 @@ using Gadgeteer.Modules.GHIElectronics;
 
 namespace ilift.Patterns
 {
+    /// <summary>
+    /// The Higher level lateral raise pattern class
+    /// </summary>
     public class LateralRaise : AbstractExercise
     {
-        
+        /// <summary>
+        /// Contains subpatterns of lateral raise
+        /// </summary>
         private ComplexPattern lateralRaisePattern;
 
+        /// <summary>
+        /// Initialize all the subpatterns
+        /// </summary>
         public LateralRaise()
         {
             lateralRaisePattern = new ComplexPattern();
@@ -19,6 +27,13 @@ namespace ilift.Patterns
 
         }
 
+        /// <summary>
+        /// The function that determines if the action is valid by passing 
+        /// the data from accelerometer to subpattern 
+        /// </summary>
+        /// <param name="x"> The x coordinator of movement
+        /// <param name="y"> The y coordinator of movement
+        /// <param name="z"> The z coordinator of movement
         public override void ProcessData(double x, double y, double z)
         {
             lateralRaisePattern.processAccelData(x, y, z);
