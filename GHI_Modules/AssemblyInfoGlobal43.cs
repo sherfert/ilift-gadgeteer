db5368308b8e78dc27bfd3d242fcf47e79d0b143
Ilmi Ali <ilmiali88@gmail.com>
2015-07-03 22:03:33 +0200

    Added Gadgeteer Tunes module

---
+++ GHI_Modules/AssemblyInfoGlobal43.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("4.3.7.9")]
+[assembly: AssemblyFileVersion("4.3.7.9")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
